5ec18de09b4252a286ceac7f12b888226ef82f55
WEBAPPICK iMac M1 - PC1 <webappick-imac-m1-pc1@WEBAPPICKs-iMac.local>
2022-02-02 21:41:43 +0600

    PageHome Complete

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-full">
+    <h2 style="text-align: center">Page not found</h2>
+    <p style="text-align: center">
+      <router-link :to="{name:'Home'}">Go to the home page</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
 import { createRouter, createWebHistory } from 'vue-router'
+
 const routes = [
   {
     path: '/',
@@ -11,6 +13,12 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
+    props: true
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound,
     props: true
   }
 ]
